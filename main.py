6ca08129b6e204b4873adf79a274ecbb897a80b6
Pat Wendorf <dungeons@gmail.com>
2023-05-25 10:42:52 -0400

    Added Splade token generation

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import numpy
 from sentence_transformers import SentenceTransformer
 from scipy.spatial.distance import euclidean
 import torch
-from transformers import BertModel, BertTokenizer
+from transformers import BertModel, BertTokenizer, AutoModelForMaskedLM, AutoTokenizer
+from splade.models.transformer_rep import Splade
 
 # Load the spacy model that you have installed
 nlp_small = spacy.load('en_core_web_sm')
@@ -19,6 +20,14 @@ st_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 bert_model = BertModel.from_pretrained('bert-base-uncased').to(device)
+
+# Splade Model
+model_type_or_dir = "naver/splade-cocondenser-ensembledistil"
+
+splade_model = Splade(model_type_or_dir, agg="max")
+splade_model.eval()
+tokenizer = AutoTokenizer.from_pretrained(model_type_or_dir)
+reverse_voc = {v: k for k, v in tokenizer.vocab.items()}
 
 # Fast API init
 app = FastAPI(
@@ -144,6 +153,25 @@ async def synonyms_medium(text: str):
 @app.get("/lsyn/")
 async def synonyms_large(text: str):
     return most_similar_words(nlp_lg, text, 10)
+
+@app.get("/splade/")
+async def splade_bow(text: str):
+    with torch.no_grad():
+        doc_rep = splade_model(d_kwargs=tokenizer(text, return_tensors="pt"))["d_rep"].squeeze()  # (sparse) doc rep in voc space, shape (30522,)
+
+    # get the number of non-zero dimensions in the rep:
+    col = torch.nonzero(doc_rep).squeeze().cpu().tolist()
+    print("number of actual dimensions: ", len(col))
+
+    # now let's inspect the bow representation:
+    weights = doc_rep[col].cpu().tolist()
+    d = {k: v for k, v in zip(col, weights)}
+    sorted_d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
+    bow_rep = []
+    for k, v in sorted_d.items():
+        bow_rep.append((reverse_voc[k], round(v, 2)))
+        
+    return bow_rep
 
 @app.get("/simdiff/")
 async def similarity_model_differences(t1: str, t2: str):
